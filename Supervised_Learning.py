64da823aa646423e59424019a57bced0f5f5dd48
krapes <kerri.rapes@gmail.com>
2017-11-13 00:11:08 -0300

    Feat: Compare all the supervised classifiers

--- Supervised_Learning.py
+++ Supervised_Learning.py
@@ -11,7 +11,15 @@ import pandas as pd
 import numpy as np
 import exploring
 from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
 
 df, features_master = exploring.preprocess_data()
@@ -23,14 +31,44 @@ for i, row in df_labeled.iterrows():
     if row.text in list(df.text):
         idx = df.index[df['text'] == row.text]
         df.set_value(idx,'cluster', row.cluster)
+        
 df = df[df.cluster != -1]
+pca = exploring.pca_components(df, features_master)
+df['pca'] = list(pca)
 
-X_train, X_test, y_train, y_test = train_test_split(list(df.features), list(df.cluster), test_size=0.33, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(list(df.features),
+                                                    list(df.cluster),
+                                                    test_size=0.33,
+                                                    random_state=42)
 
-print(len(df))
-print(len(X_train), len(X_test), len(y_train), len(y_test))
+names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
+         "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
+         "Naive Bayes", "QDA"]
 
-clf = DecisionTreeClassifier(random_state=0)
-clf.fit(X_train, y_train)
-print(clf.score(X_test, y_test))
+classifiers = [
+    KNeighborsClassifier(3),
+    SVC(kernel="linear", C=0.025),
+    SVC(gamma=2, C=1),
+    GaussianProcessClassifier(1.0 * RBF(1.0)),
+    DecisionTreeClassifier(max_depth=5),
+    RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+    MLPClassifier(alpha=1),
+    AdaBoostClassifier(),
+    GaussianNB(),
+    QuadraticDiscriminantAnalysis()]
 
+score_sum = 0
+best_score = 0
+best_classifier = ""
+for name, clf in zip(names, classifiers):
+        clf.fit(X_train, y_train)
+        score = clf.score(X_test, y_test)
+        score_sum += score
+        if score > best_score:
+            best_score = score
+            best_classifier = name
+        print("{} scored {}".format(name, score))
+
+print("")
+print("The average score was {}".format(score_sum/len(names)))
+print("The best classifier was {} with a score of {}".format(best_classifier, best_score))
