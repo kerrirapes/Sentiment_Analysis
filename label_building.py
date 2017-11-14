a6df077af4113860f5abedccc96d5a75d4cae540
krapes <kerri.rapes@gmail.com>
2017-11-14 15:54:02 -0300

    Feat: Modify pruning to include words only found in one group

--- label_building.py
+++ label_building.py
@@ -17,13 +17,33 @@ import random
 from collections import Counter
 
 
-for _ in range(2):
+
+
+
+
+df_labeled = pd.read_pickle('labeled.pkl')
+for _ in range(100):
     df, features_master = exploring.preprocess_data()
     df["cluster"] = -1.0
+    df_orgional = df.copy()
     
-    df_labeled = pd.read_pickle('labeled.pkl')
     df_ml = pd.read_pickle('machine_labeled.pkl')
-    #df = df_labeled
+    #df_ml = df_ml[df_ml.index == 0]
+    print("Machine Labeled Messages Size: {}".format(len(df_ml[df_ml.cluster != -1])))
+    
+    for label in [df_labeled]:
+        for i, row in label.iterrows():
+            if row.text in list(df_orgional.text):
+                idx = df_orgional.index[df['text'] == row.text]
+                df_orgional.set_value(idx,'cluster', row.cluster)
+
+    df_train =  df_orgional[df_orgional.cluster != -1]       
+        
+    X_trainO, X_testO, y_trainO, y_testO = train_test_split(list(df_train.features),
+                                                        list(df_train.cluster),
+                                                        test_size=0.75,
+                                                        random_state=42)
+
     for label in [df_labeled, df_ml]:
         for i, row in label.iterrows():
             if row.text in list(df.text):
@@ -51,8 +71,11 @@ for _ in range(2):
             clf.fit(X_train, y_train)
             score = clf.score(X_test, y_test)
             print("Score for {} was {}".format(name, round(score,2)))
+    for name, clf in zip(names, classifiers):
+        score = clf.score(X_testO, y_testO)
+        print("The real score for {} was {}".format(name, round(score,2)))
     
-    indexes = random.sample(range(len(df)), min(int(len(df)*.75), 100))       
+    indexes = random.sample(range(len(df)), int(len(df)*.75))       
     for i in indexes:
         message = df.iloc[i].features
         predictions = []
@@ -74,3 +97,28 @@ for _ in range(2):
         df_ml.to_pickle('machine_labeled.pkl')
     except:
         df.to_pickle('machine_labeled.pkl')
+
+try:
+    cgroups = range(2)
+    for cgroup in cgroups:
+        print("Messages from group {}".format(cgroup))
+        for message in df.groupby('cluster').get_group(cgroup).text.head(20):
+            print(message)
+        print("")
+except:
+    print("Only one group")
+    
+    
+
+
+names = ["Linear SVM",  "Neural Net", "AdaBoost"]
+
+classifiers = [
+    SVC(kernel="linear", C=0.025),
+    MLPClassifier(alpha=1),
+    AdaBoostClassifier()]
+
+
+
+
+        
--- pruning_dict.py
+++ pruning_dict.py
@@ -14,7 +14,7 @@ def build_vocabulary(messages, word_drop=True):
         for message in messages:
             message = remove_nonalphanumeric(message)
             message_split = message.split()
-            gram_count = 3
+            gram_count = 2
             grams = []
             for g in range(gram_count):
                 for i in range(len(message_split) - 1):
@@ -41,7 +41,7 @@ def remove_nonalphanumeric(message):
     
 def prune_vocab(vocabulary):
     #try:
-    labels = ['machine_labeled.pkl', 'labeled.pkl']
+    labels = [ 'labeled.pkl']
     df = pd.DataFrame()
     for l in labels:
         df_temp = pd.read_pickle(l)
@@ -49,22 +49,38 @@ def prune_vocab(vocabulary):
         df = df.drop_duplicates(subset='text', keep="first")
     df = df.sort_values(by=['cluster'], ascending=False)
     df =  df[df.cluster != -1]
-    df = df[pd.notnull(df['features'])]
+    df = df[pd.notnull(df['text'])]
     df = df.reset_index(drop=True)
 
     
     info = df.groupby('cluster').get_group(0)
     info_v = build_vocabulary(info.text, word_drop=True)
+    info_w = info_v.keys()
     express = df.groupby('cluster').get_group(1)
     express_v = build_vocabulary(express.text, word_drop=True)
-    common = info_v & express_v
-    # TODO add polarizing words that are not common
+    express_w = express_v.keys()
     
-    words = list(common.keys())
+    common_v = info_v & express_v
+    common_w = common_v.keys()
+    
+    words = []
     ratios = []
-    for word in list(words):
-        ratios.append(info_v[word] / express_v[word])
     
+    for word in info_w:
+        if word not in common_w:
+            ratios.append(info_v[word])
+            words.append(word)
+    
+    for word in common_w:
+        ratios.append(info_v[word] / express_v[word])
+        words.append(word)
+    
+    for word in express_w:
+        if word not in common_w:
+            ratios.append(express_v[word] * -1)
+            words.append(word)
+    
+
     threshold = int(len(words) * .15)
     top20 = np.argsort(ratios)[-threshold:]
     bottom20 = np.argsort(ratios)[:threshold]
@@ -73,6 +89,7 @@ def prune_vocab(vocabulary):
     for group in [top20, bottom20]:
         for index in group:
             polar_words.append(words[index])
+    
     
     vocabulary = Counter(polar_words) & vocabulary
     #print(Counter(polar_words))
