6100b463d75efe540bbe8b92fa93f2b8aff5981e
krapes <kerri.rapes@gmail.com>
2017-11-15 00:46:16 -0300

    Feat: Add validation set to label_building

--- label_building.py
+++ label_building.py
@@ -7,7 +7,6 @@ Polling Supervised Learnt Algorthims to build labels
 """
 
 import pandas as pd
-import numpy as np
 import exploring
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
@@ -18,12 +17,24 @@ import time
 import os
 
 
+def relate_dfs(df_features, df_clusters):
+    for label in df_clusters:
+        for i, row in label.iterrows():
+            if row.text in list(df_features.text):
+                idx = df_features.index[df_features['text'] == row.text]
+                df_features.set_value(idx,'cluster', row.cluster)
+    
+    print(df.groupby('cluster').count())
+    return df_features
 
 
 start = time.time()
 
 df_labeled = pd.read_pickle('labeled.pkl')
-first_run = True
+df_labeled = df_labeled[df_labeled.cluster != -1]
+df_labeled, df_validation = train_test_split(df_labeled, test_size=0.10)
+
+
 LM_final = 1
 LM_previous = 0
 count = 0 
@@ -33,38 +44,24 @@ while LM_final - LM_previous > 0:
     
     df_ml = pd.read_pickle('machine_labeled.pkl')
     if count <= 1:
-        df_ml = df_ml[df_ml.index == 0]
+        #df_ml = df_ml[df_ml.index == 0]
+        df_ml = pd.DataFrame(columns=['text', 'features', 'cluster'])
         df_ml.to_pickle('machine_labeled.pkl')
     LM_previous = len(df_ml[df_ml.cluster != -1])
     print("Machine Labeled Messages Size: {}".format(LM_previous))
     
     df, features_master = exploring.preprocess_data()
     df["cluster"] = -1.0
-    df_orgional = df.copy()
     
+    #df_orgional = df.copy()
     
-    
-    for label in [df_labeled]:
-        for i, row in label.iterrows():
-            if row.text in list(df_orgional.text):
-                idx = df_orgional.index[df['text'] == row.text]
-                df_orgional.set_value(idx,'cluster', row.cluster)
-
-    df_train =  df_orgional[df_orgional.cluster != -1]       
-        
-    X_trainO, X_testO, y_trainO, y_testO = train_test_split(list(df_train.features),
-                                                        list(df_train.cluster),
-                                                        test_size=0.75,
-                                                        random_state=42)
-
-    for label in [df_labeled, df_ml]:
-        for i, row in label.iterrows():
-            if row.text in list(df.text):
-                idx = df.index[df['text'] == row.text]
-                df.set_value(idx,'cluster', row.cluster)
+    df_validation = relate_dfs(df, [df_validation])
+    df_validation = df_validation.copy()
     print(df.groupby('cluster').count())
+    df = relate_dfs(df, [df_labeled, df_ml])
+
     df_train =  df[df.cluster != -1]       
-    df = df[df.cluster == -1]
+    
 
     
     X_train, X_test, y_train, y_test = train_test_split(list(df_train.features),
@@ -86,7 +83,7 @@ while LM_final - LM_previous > 0:
             score = clf.score(X_test, y_test)
             print("Score for {} was {}".format(name, round(score,2)))
     for name, clf in zip(names, clfs):
-        score = clf.score(X_testO, y_testO)
+        score = clf.score(list(df_validation.features), list(df_validation.cluster))
         print("The real score for {} was {}".format(name, round(score,2)))
     
     #indexes = random.sample(range(len(df)), int(len(df)))       
